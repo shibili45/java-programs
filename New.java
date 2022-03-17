3f9f36d02118cdf781783673ae1f3f2dab20b9cc
Mohammed Shibili <96052291+shibili45@users.noreply.github.com>
2022-03-17 16:09:24 +0530

    Add files via upload

---
+++ New.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ class New {
+	public static void main(String[] args)
+	{
+	Scanner sc= new Scanner(System.in);
+	System.out.println("Student details");
+	System.out.println("enter name:");
+	String Name = sc.nextLine();
+	System.out.println("enter roll number:");
+	int rollno = sc.nextInt();
+	System.out.println("enter mark:");
+	int mark=sc.nextInt();
+	
+	System.out.println(Name);
+	
+	System.out.println(rollno);
+	
+	System.out.println(mark);
+	
+	}
+
+}
